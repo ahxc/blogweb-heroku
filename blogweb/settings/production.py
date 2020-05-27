0a0e61e52b67766b004c803295320cd46c57be78
aha <43837296+ahxc@users.noreply.github.com>
2020-05-27 20:18:14 +0800

    Update production.py

--- blogweb/settings/production.py
+++ blogweb/settings/production.py
@@ -6,7 +6,7 @@ from .common import *
 #SECRET_KEY = production_secret_key
 SECRET_KEY = 'n9ceqv38)#&mwuat@(mjb_p%em$e8$qyr#fw9ot!=ba6lijx-6'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['blogweb-django.herokuapp.com']
 
